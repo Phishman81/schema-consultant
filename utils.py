63bfcc5b577583115c6f3d3ae9ea40cdec136f83
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-19 00:27:37 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,48 +1,58 @@
 import requests
-from bs4 import BeautifulSoup
-import json
 import openai
+from bs4 import BeautifulSoup
+from extruct.jsonld import JsonLdExtractor
 
 def fetch_html(url):
     response = requests.get(url)
     return response.text
 
-def extract_text(html):
-    soup = BeautifulSoup(html, 'html.parser')
+def extract_text(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
     text = soup.get_text()
     return text
 
-def extract_json_ld(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    json_ld = [json.loads(script.string) for script in soup.find_all('script', {'type':'application/ld+json'})]
-    return json_ld
+def extract_json_ld(html_content):
+    extractor = JsonLdExtractor()
+    data = extractor.extract(html_content)
+    return data
 
 def analyze_text_with_gpt(api_key, text):
-    # Set the OpenAI API key
     openai.api_key = api_key
+    system_msg = 'You are a helpful assistant who understands schema markup and SEO.'
+    user_msg = f'Please analyze the following text and suggest possible schema properties: {text}'
 
-    # Set the system message
-    system_msg = 'You are an assistant that understands language and data.'
+    response = openai.ChatCompletion.create(model="gpt-4-0613",
+                                            messages=[{"role": "system", "content": system_msg},
+                                                      {"role": "user", "content": user_msg}])
+    return response["choices"][0]["message"]["content"]
 
-    # Set the user message
-    user_msg = text
+def create_schema_with_gpt(api_key, properties):
+    openai.api_key = api_key
+    system_msg = 'You are a knowledgeable assistant capable of creating schema markup.'
+    user_msg = f'Please create a schema markup with the following properties: {properties}'
 
-    # Create a request to the GPT API
-    response = openai.ChatCompletion.create(
-        model="gpt-4-0613",
-        messages=[
-            {"role": "system", "content": system_msg},
-            {"role": "user", "content": user_msg}
-        ]
-    )
+    response = openai.ChatCompletion.create(model="gpt-4-0613",
+                                            messages=[{"role": "system", "content": system_msg},
+                                                      {"role": "user", "content": user_msg}])
+    return response["choices"][0]["message"]["content"]
 
-    # Extract the content from the response
-    content = response["choices"][0]["message"]["content"]
-    return content
+def combine_schemas_with_gpt(api_key, existing_schema, new_schema):
+    openai.api_key = api_key
+    system_msg = 'You are an assistant skilled in combining and optimizing schema markups.'
+    user_msg = f'Please combine the following schema markups and optimize them: {existing_schema}, {new_schema}'
 
-def enhance_schema(json_ld, gpt_response):
-    # For now, we're simply adding the GPT response as a potential enhancement.
-    # In a real-world scenario, this would involve parsing the GPT response and adding relevant information to the schema.
-    for schema in json_ld:
-        schema['gpt_enhancement'] = gpt_response
-    return json_ld
+    response = openai.ChatCompletion.create(model="gpt-4-0613",
+                                            messages=[{"role": "system", "content": system_msg},
+                                                      {"role": "user", "content": user_msg}])
+    return response["choices"][0]["message"]["content"]
+
+def explain_changes_with_gpt(api_key, original_schema, enhanced_schema):
+    openai.api_key = api_key
+    system_msg = 'You are an assistant capable of explaining changes in schema markups.'
+    user_msg = f'Please explain the changes between the original schema {original_schema} and the enhanced schema {enhanced_schema}'
+
+    response = openai.ChatCompletion.create(model="gpt-4-0613",
+                                            messages=[{"role": "system", "content": system_msg},
+                                                      {"role": "user", "content": user_msg}])
+    return response["choices"][0]["message"]["content"]
