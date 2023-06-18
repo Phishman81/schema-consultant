50fe1ce1388ffca1f384e0df074ec137d1dc6b6e
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-19 00:28:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,37 @@
 import streamlit as st
-from utils import fetch_html, extract_text, extract_json_ld, analyze_text_with_gpt, enhance_schema
-import logging
+from utils import (fetch_html, extract_text, extract_json_ld, analyze_text_with_gpt,
+                   create_schema_with_gpt, combine_schemas_with_gpt, explain_changes_with_gpt)
 
-logging.basicConfig(level=logging.INFO)
+st.title("Schema Markup Consultant")
 
-def app():
-    st.title("Schema Markup Consultant")
+api_key = st.text_input("Enter your GPT-API key:")
 
-    api_key = st.text_input("Enter your GPT API key")
+url = st.text_input("Enter a URL:")
+if url:
+    html_content = fetch_html(url)
+    text_content = extract_text(html_content)
+    st.write("Here is the continuation of the `app.py` file:
 
-    url = st.text_input("Enter a URL")
-    if url:
-        try:
-            html = fetch_html(url)
-            text = extract_text(html)
-            st.text(text)
-
-            st.text("Now looking for JSON-LD schema markup.")
-            try:
-                json_ld = extract_json_ld(html)
-                st.markdown("**Found schema markup:**")
-                st.json(json_ld)
-            except Exception as e:
-                logging.error(f"Exception occurred while finding schema markup: {e}")
-                st.error(f"An error occurred: {e}")
-
-            st.text("Now analyzing text content to identify possible schema properties.")
-            try:
-                gpt_response = analyze_text_with_gpt(api_key, text)
-                st.text(gpt_response)
-            except Exception as e:
-                logging.error(f"Exception occurred while analyzing text: {e}")
-                st.error(f"An error occurred: {e}")
-
-            st.text("Now trying to combine the current schema markup with the new findings.")
-            try:
-                enhanced_json_ld = enhance_schema(json_ld, gpt_response)
-                st.json(enhanced_json_ld)
-            except Exception as e:
-                logging.error(f"Exception occurred while enhancing schema: {e}")
-                st.error(f"An error occurred: {e}")
-
-        except Exception as e:
-            logging.error(f"Exception occurred while processing URL: {e}")
-            st.error(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    app()
+```python
+    st.write("Here is the extracted text content:")
+    st.write(text_content)
+    
+    schema_properties = analyze_text_with_gpt(api_key, text_content)
+    st.write("Here are the identified schema properties:")
+    st.write(schema_properties)
+    
+    new_schema = create_schema_with_gpt(api_key, schema_properties)
+    st.write("Here is the new schema markup created:")
+    st.write(new_schema)
+    
+    existing_schema = extract_json_ld(html_content)
+    st.write("Here is the existing schema markup found on the page:")
+    st.write(existing_schema)
+    
+    enhanced_schema = combine_schemas_with_gpt(api_key, existing_schema, new_schema)
+    st.write("Here is the enhanced schema markup after combining the existing and new schema markups:")
+    st.write(enhanced_schema)
+    
+    explanation = explain_changes_with_gpt(api_key, existing_schema, enhanced_schema)
+    st.write("Here is an explanation of the changes and optimizations made:")
+    st.write(explanation)
