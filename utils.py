f9447a5555b926964278d12d22e1b67175285596
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-18 23:55:02 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,28 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-from openai import OpenAI, GPT3Encoder
+import openai
 
 def fetch_html(url):
     response = requests.get(url)
     return response.text
 
-def extract_text(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    return soup.get_text()
+def extract_text(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.get_text()
+    return text
 
-def extract_json_ld(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    json_ld_data = [json.loads(script.string) for script in soup.find_all('script', type='application/ld+json')]
-    return json_ld_data
+def extract_json_ld(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    json_ld = [json.loads(script.string) for script in soup.find_all('script', {'type':'application/ld+json'})]
+    return json_ld
 
-def analyze_text_with_gpt(text, openai_api_key):
-    openai.api_key = openai_api_key
-    model = "gpt-4-0613"
-    response = openai.Completion.create(model=model, prompt=text, max_tokens=60)
-    return response.choices[0].text.strip()
+def analyze_text_with_gpt(api_key, text):
+    # Set the OpenAI API key
+    openai.api_key = api_key
 
-def enhance_schema(json_ld_data, new_properties):
-    # Here is where you would add the logic to combine the existing JSON-LD data with the new properties
-    pass
+    # Set the system message
+    system_msg = 'You are an assistant that understands language and data.'
 
+    # Set the user message
+    user_msg = text
+
+    # Create a request to the GPT API
+    response = openai.ChatCompletion.create(
+                    model="gpt-4-0613",
+                    messages=[{"role": "system", "content": system_msg},
+                                {"role": "user", "content": user_msg}])
+    
+    # Extract the content from the response
+    content = response["choices"][0]["message"]["content"]
+    return content
+
+def enhance_schema(json_ld, gpt_response):
+    # For now, we're simply adding the GPT response as a potential enhancement.
+    # In a real-world scenario, this would involve parsing the GPT response and adding relevant information to the schema.
+    for schema in json_ld:
+       
