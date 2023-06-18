8087a6277eb94ce703d7aef9b352e4e7cde45eeb
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-18 23:46:08 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from openai import OpenAI, GPT3Encoder
+
+def fetch_html(url):
+    response = requests.get(url)
+    return response.text
+
+def extract_text(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    return soup.get_text()
+
+def extract_json_ld(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    json_ld_data = [json.loads(script.string) for script in soup.find_all('script', type='application/ld+json')]
+    return json_ld_data
+
+def analyze_text_with_gpt(text, openai_api_key):
+    openai.api_key = openai_api_key
+    model = "gpt-4-0613"
+    response = openai.Completion.create(model=model, prompt=text, max_tokens=60)
+    return response.choices[0].text.strip()
+
+def enhance_schema(json_ld_data, new_properties):
+    # Here is where you would add the logic to combine the existing JSON-LD data with the new properties
+    pass
 
